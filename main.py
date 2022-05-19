b34bbd2d1494e8f5d8bebe6ebbb0166dd9ccfd09
Joseph <sealsuarez@gmail.com>
2022-05-19 13:50:08 -0400

    Delete old helper fns

--- main.py
+++ main.py
@@ -35,68 +35,6 @@ else:
     from config.cleanrl import Train
     from config.cleanrl import Eval
 
-def pack_indices(dones):
-    steps, ents = dones.shape
-    flat_indices, traj_lens = [], []
-    idx = 0
-    for ent in range(ents):
-        read_zero = False
-        traj = []
-
-        for step in range(steps):
-            # Got a step of agent data
-            if dones[step][ent] == 0:
-                traj.append(idx)
-                read_zero = True
-
-            #Trajectory bound
-            elif dones[step][ent] == 1 and read_zero:
-                traj_lens.append(len(traj))
-                flat_indices += traj
-
-                read_zero = False
-                traj = []
-
-            idx += 1
-
-        if read_zero:
-            traj_lens.append(len(traj))
-            flat_indices += traj
-
-    assert len(flat_indices) == sum(traj_lens), f'{len(trajectories)}, {sum(traj_lens)}'
-    return flat_indices, traj_lens
-
-
-def pad_to_pack(tensor, dones):
-    steps, ents = dones.shape
-    trajectories = []
-    traj_lens = []
-    for ent in range(ents):
-        read_zero = False
-        traj = []
-
-        for step in range(steps):
-            # Got a step of agent data
-            if dones[step][ent] == 0:
-                traj.append(tensor[step][ent])
-                read_zero = True
-
-            #Trajectory bound
-            elif dones[step][ent] == 1 and read_zero:
-                traj_lens.append(len(traj))
-                trajectories += traj
-
-                read_zero = False
-                traj = []
-
-        if read_zero:
-            traj_lens.append(len(traj))
-            trajectories += traj
-
-    assert len(trajectories) == sum(traj_lens), f'{len(trajectories)}, {sum(traj_lens)}'
-    return torch.stack(trajectories), traj_lens
-
-
 class Agent(nn.Module):
     def __init__(self, config):
         super().__init__()
