936b26e96504537a44017505f939ddde44d18184
David Bloomin <daveey@gmail.com>
2023-03-21 15:15:56 -0700

    start implementation of baseline_env.py

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
 import torch
+from baseline_env import BaselineEnv
 import cleanrl_ppo_lstm
 import pufferlib.emulation
 import pufferlib.registry.nmmo
 import pufferlib.frameworks.cleanrl
 import nmmo
-from env.team_env import TeamEnv
+from team_env import TeamEnv
 from model.policy import Policy
 
 def create_env():
-    nmmo_env = nmmo.Env()
-    teams = [[player for player in range(team * 8 + 1, (team + 1) * 8 + 1)] for team in range(16)]
-    team_env = TeamEnv(nmmo_env, teams)
-    return team_env
+  nmmo_env = nmmo.Env()
+  teams = [[player for player in range(team * 8 + 1, (team + 1) * 8 + 1)] for team in range(16)]
+  env = BaselineEnv(nmmo_env, teams)
+  return env
 
 if __name__ == "__main__":
-    num_cores = 1
+  num_cores = 1
 
-    binding = pufferlib.emulation.Binding(
-        env_creator=create_env,
-        env_name="Neural MMO",
-    )
+  binding = pufferlib.emulation.Binding(
+    env_creator=create_env,
+    env_name="Neural MMO",
+  )
 
-    agent = pufferlib.frameworks.cleanrl.make_policy(Policy, lstm_layers=1)(
-        binding
-    )
+  agent = pufferlib.frameworks.cleanrl.make_policy(Policy, lstm_layers=1)(
+    binding
+  )
 
-    assert binding is not None
-    cleanrl_ppo_lstm.train(
-        binding,
-        agent,
-        cuda=torch.cuda.is_available(),
-        total_timesteps=10_000_000,
-        track=True,
-        num_envs=num_cores,
-        num_cores=num_cores,
-        num_buffers=4,
-        num_minibatches=4,
-        num_agents=16,
-        wandb_project_name="pufferlib",
-        wandb_entity="platypus",
-    )
+  assert binding is not None
+  cleanrl_ppo_lstm.train(
+    binding,
+    agent,
+    cuda=torch.cuda.is_available(),
+    total_timesteps=10_000_000,
+    track=True,
+    num_envs=num_cores,
+    num_cores=num_cores,
+    num_buffers=4,
+    num_minibatches=4,
+    num_agents=16,
+    wandb_project_name="pufferlib",
+    wandb_entity="platypus",
+  )
---
+++ team_env.py
@@ -0,0 +1,91 @@
+from collections import defaultdict
+from typing import Any, Dict, List
+
+import gym
+from pettingzoo.utils.env import AgentID, ParallelEnv
+
+class TeamEnv(ParallelEnv):
+  def __init__(self, env: ParallelEnv, teams: List[List[int]]):
+    self._env = env
+    self._teams = teams
+
+    self.possible_agents = list(range(len(teams)))
+
+    self._team_size = {}
+    self._team_and_pos_for_agent = {}
+    self._position_for_agent = {}
+    self._agent_for_team_and_position = {}
+
+    for team_id, team in enumerate(teams):
+      self._team_size[team_id] = len(team)
+      for position, agent_id in enumerate(team):
+        self._team_and_pos_for_agent[agent_id] = (team_id, position)
+        self._agent_for_team_and_position[team_id, position] = agent_id
+
+  def action_space(self, team: int) -> gym.Space:
+    return gym.spaces.Dict({
+      pos: self._env.action_space(self._agent_for_team_and_position[team, pos]) \
+        for pos in range(self._team_size[team])
+    })
+
+  def observation_space(self, team: int) -> gym.Space:
+    return gym.spaces.Dict({
+      pos: self._env.observation_space(self._agent_for_team_and_position[team, pos]) \
+        for pos in range(self._team_size[team])
+    })
+
+  def _group_by_team(self, data: Dict[int, Any]) -> Dict[int, Dict[int, Any]]:
+    grouped_data = defaultdict(dict)
+    for agent_id, value in data.items():
+      team_id, pos = self._team_and_pos_for_agent[agent_id]
+      grouped_data[team_id][pos] = value
+    return dict(grouped_data)
+
+  def _team_actions_to_agent_actions(self, team_actions: Dict[int, Dict[int, Any]]) -> Dict[int, Any]:
+    agent_actions = {}
+    for team_id, team_action in team_actions.items():
+      for pos, action in team_action.items():
+        agent_id = self._agent_for_team_and_position[team_id, pos]
+        agent_actions[agent_id] = action
+    return agent_actions
+
+  def reset(self, **kwargs) -> Dict[int, Any]:
+    gym_obs = self._env.reset(**kwargs)
+    return self._group_by_team(gym_obs)
+
+  def step(self, actions: Dict[int, Dict[str, Any]]):
+    agent_actions = self._team_actions_to_agent_actions(actions)
+    gym_obs, rewards, dones, infos = self._env.step(agent_actions)
+    merged_obs = self._group_by_team(gym_obs)
+    merged_rewards = {
+      tid: sum(vals.values()) for tid, vals in self._group_by_team(rewards).items()
+    }
+    merged_infos = self._group_by_team(infos)
+    merged_dones = {
+      tid: all(vals.values()) for tid, vals in self._group_by_team(dones).items()
+    }
+    return merged_obs, merged_rewards, merged_dones, merged_infos
+
+  ############################################################################
+  # PettingZoo API
+  ############################################################################
+
+  def render(self, mode='human'):
+    return self._env.render(mode)
+
+  @property
+  def agents(self) -> List[AgentID]:
+    return self.possible_agents
+
+  def close(self):
+    return self._env.close()
+
+  def seed(self, seed=None):
+    return self._env.seed(seed)
+
+  def state(self):
+    return self._env.state()
+
+  @property
+  def metadata(self) -> Dict:
+    return self._env.metadata
