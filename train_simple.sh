ca1f110c38ea0d214d641260a7de07d247a256bb
David Bloomin <daveey@gmail.com>
2023-04-26 11:59:06 -0700

    cleanup args

---
+++ train_simple.sh
@@ -0,0 +1,12 @@
+python -O -m main \
+--model.arch=simple \
+--env.num_teams=8 \
+--envteam_size=1 \
+--rollout.num_envs=1 \
+--rollout.num_buffers=1  \
+--rollout.num_steps=128 \
+--wandb.entity=daveey \
+--wandb.project=nmmo \
+--checkpoint_dir=/fsx/home-daveey/checkpoints/simple.1 \
+--resume_from=latest \
+--train.num_steps=100000000
