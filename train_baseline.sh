03469d6da0eb71106799f2f65371f6578dce91da
David Bloomin <daveey@gmail.com>
2023-04-26 13:01:48 -0700

    cleanup args

--- train_baseline.sh
+++ train_baseline.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 python -O -m main \
 --rollout.num_envs=4 \
 --rollout.num_buffers=4  \
 --rollout.num_steps=32 \
 --wandb.entity=daveey \
 --wandb.project=nmmo \
---checkpoint_dir=/fsx/home-daveey/checkpoints/baseline \
---resume_from=latest \
---train.num_steps=100000000
+--experiments_dir=/fsx/home-daveey/experiments \
+--train.num_steps=100000000 \
+"$@"
