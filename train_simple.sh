78e2b97ea493bb5d819c3b59cf00b34aacd1c4ab
David Bloomin <daveey@gmail.com>
2023-04-27 13:29:16 -0700

    food rewards

--- train_simple.sh
+++ train_simple.sh
@@ -17,21 +17,34 @@
 job_name=$3
 #SBATCH --job-name="$job_name"
 
-source /fsx/home-daveey/miniconda3/etc/profile.d/conda.sh && \
-conda activate nmmo && \
-ulimit -c unlimited && \
-ulimit -s unlimited && \
-ulimit -a && \
-stdbuf -oL -eL python -u -O -m main \
---model.arch=simple \
---env.num_teams=8 \
---env.team_size=1 \
---rollout.num_envs=1 \
---rollout.num_buffers=1  \
---rollout.num_steps=128 \
---wandb.entity=daveey \
---wandb.project=nmmo \
---train.experiments_dir=/fsx/home-daveey/experiments \
---train.num_steps=100000000 \
---train.experiment_name="$1" \
-"${@:3}"
+while true; do
+
+  source /fsx/home-daveey/miniconda3/etc/profile.d/conda.sh && \
+  conda activate nmmo && \
+  ulimit -c unlimited && \
+  ulimit -s unlimited && \
+  ulimit -a && \
+  stdbuf -oL -eL python -u -O \
+  -m main \
+  --model.arch=simple \
+  --env.num_teams=8 \
+  --env.team_size=1 \
+  --rollout.num_envs=1 \
+  --rollout.num_buffers=1  \
+  --rollout.num_steps=128 \
+  --wandb.entity=daveey \
+  --wandb.project=nmmo \
+  --train.experiments_dir=/fsx/home-daveey/experiments \
+  --train.num_steps=100000000 \
+  --train.experiment_name="$1" \
+  "${@:3}"
+
+  exit_status=$?
+
+  if [ $exit_status -eq 0 ]; then
+    echo "Job completed successfully."
+    break
+  else
+    echo "Job failed with exit status $exit_status. Retrying..."
+  fi
+done
