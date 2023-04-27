74f3cca58c38ffb933ffb0db63dfcf3800de4a73
David Bloomin <daveey@gmail.com>
2023-04-26 18:40:35 -0700

    cleanup args

---
+++ batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --comment=carperai
+#SBATCH --partition=g40
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=6
+#SBATCH --chdir=/fsx/home-daveey/nmmo-baselines/
+#SBATCH --output=job_output_%j.txt
+#SBATCH --error=job_error_%j.txt
+#SBATCH --requeue
+
+"$@"
