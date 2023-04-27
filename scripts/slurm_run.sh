46b14457bce257025a86972d13d51890a0e3adf2
David Bloomin <daveey@gmail.com>
2023-04-27 13:43:51 -0700

    add scripts

--- scripts/slurm_run.sh
+++ scripts/slurm_run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Usage: sbatch train_simple.sh experiment_name --arg1=value1 --arg2=value2 ...
-
-
 #SBATCH --comment=carperai
 #SBATCH --partition=g40
 #SBATCH --nodes=1
@@ -14,21 +11,22 @@
 #SBATCH --error=sbatch/%j.error
 #SBATCH --requeue
 #SBATCH --export=PYTHONUNBUFFERED=1
-job_name=$3
-#SBATCH --job-name="$job_name"
 
 source /fsx/home-daveey/miniconda3/etc/profile.d/conda.sh && \
 conda activate nmmo && \
 ulimit -c unlimited && \
 ulimit -s unlimited && \
-ulimit -a && \
-stdbuf -oL -eL python -u -O -m main \
---rollout.num_envs=4 \
---rollout.num_buffers=4  \
---rollout.num_steps=32 \
---wandb.entity=daveey \
---wandb.project=nmmo \
---train.experiments_dir=/fsx/home-daveey/experiments \
---train.num_steps=100000000 \
---train.experiment_name="$1" \
-"${@:3}"
+ulimit -a
+
+while true; do
+  stdbuf -oL -eL python -u -O "$@"
+
+  exit_status=$?
+
+  if [ $exit_status -eq 0 ]; then
+    echo "Job completed successfully."
+    break
+  else
+    echo "Job failed with exit status $exit_status. Retrying..."
+  fi
+done
---
+++ scripts/train_baseline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python -u -O -m main \
+--rollout.num_envs=4 \
+--rollout.num_buffers=4  \
+--rollout.num_steps=32 \
+--wandb.entity=daveey \
+--wandb.project=nmmo \
+--train.experiments_dir=/fsx/home-daveey/experiments \
+--train.num_steps=100000000 \
+"${@}"
