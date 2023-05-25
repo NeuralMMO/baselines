0ea578609c649a8409e03a7895fd6189c3c71709
jsuarez5341 <sealsuarez@gmail.com>
2023-05-25 02:54:28 +0000

    Simple model. Tested without self emb. This one has self emb

--- train.py
+++ train.py
@@ -21,6 +21,8 @@ from model.realikun.policy import BaselinePolicy
 from env.nmmo_team_env import NMMOTeamEnv
 from lib.team.team_helper import TeamHelper
 
+import logging
+
 if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO)
 
