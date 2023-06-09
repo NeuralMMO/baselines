733709d88bab4af923d44cfa22017901cbc5c485
kywch <choe.kyoung@gmail.com>
2023-06-09 18:30:24 +0000

    updated the end-to-end curriculum training loop

--- tests/test_curriculum_track.py
+++ tests/test_curriculum_track.py
@@ -4,18 +4,50 @@
 import wandb
 
 from nmmo.core.config import Config
+import nmmo.task.base_predicates as prebuilt_eval_fn
 
-from curriculum.heuristic_task_gen import HeuristicTaskGenerator
+import curriculum.submission_eval_fn as participant_eval_fn
+import curriculum.submission_curriculum as participant_curriculum
+import curriculum.manual_curriculum as eval_curriculum
+
+
+# Task spec, which is a list tuple and pickable, is passed around
+# task_spec: (reward_to, eval_fn, kwargs)
+# task_spec_with_embedding: (reward_to, eval_fn, kwargs, task_embedding)
+
+# assuming something like this
+class TaskEmbeddingGenerator:
+  def __init__(self, model, contexts):
+    # hopefully, the model is stored locally and also used by OpenELM
+    self.model = model
+
+    # the context is the eval function definitions, both pre-built and submitted
+    self.prompt_template = self._contruct_prompt_template(contexts)
+
+  def _contruct_prompt_template(self, context):
+    pass
+
+  def _construct_prompt(self, reward_to, eval_fn, eval_fn_kwargs):
+    pass
+
+  def get_task_embedding(self, task_spec):
+    task_embedding = []
+    for reward_to, eval_fn, kwargs in task_spec:
+      prompt = self._construct_prompt(reward_to, eval_fn, kwargs)
+      embedding = self.model.get_hidden_layer(prompt) # something like this? 
+      task_embedding.append(embedding)
+    return task_embedding
+
 
 # to be provided by Joseph
 #from cleanrl_ppo_lstm import train_on_tasks, evaluate_on_tasks
-def train_on_tasks():
+def train_on_tasks(agent_mode, task_spec_with_embedding):
   pass
 
-def evaluate_on_tasks():
+def evaluate_on_tasks(agent_mode, task_spec_with_embedding):
   pass
 
-def load_model(pretrained_model_path):
+def load_agent_model(model_path):
   pass
 
 # Nishaanth's OpenELM task generator
@@ -29,51 +61,65 @@ epochs_per_elm_update = 10
 # Ryan's syllabus task sampler
 #from curriculum.syllabus_task_sampler.py_import SyllabusTaskSampler
 class SyllabusTaskSampler:
-  def update_tasks(): # please suggest better name
+  def add_new_tasks(self, task_spec_with_embedding):
     pass
 
   def sample_tasks(self, num_tasks):
+    pass # return task_spec_with_embedding
+
+  def update(self, task_spec_with_embedding, train_stats):
     pass
 
-  def update(self, batch_of_tasks, train_stats):
-    pass
 
+#####################################################################
+# this is the main training loop
+
+# LLM model path, which is shared by OpenELM and the task embedding generator
+LLM_MODEL_PATH = '' 
+
+# Joseph's comment: this will likely have to work off of a pretrained model
+AGENT_MODEL_PATH = ''
+agent_model = load_agent_model(AGENT_MODEL_PATH)
 
 def test_curriculum_learning(config: Config, use_elm=True):
-  generator = HeuristicTaskGenerator(config)
-  train_tasks = generator.generate_tasks(num_tasks=25_000)
-  eval_tasks = generator.generate_tasks(num_tasks=1_000)
+  # eval fn definitions are provided as the context for LLM
+  task_encoder = TaskEmbeddingGenerator(LLM_MODEL_PATH, [prebuilt_eval_fn, participant_eval_fn])
+
+  train_task_spec = participant_curriculum.task_spec
+  train_task_spec_with_embedding = task_encoder.get_task_embedding(train_task_spec)
+  task_sampler = SyllabusTaskSampler(train_task_spec_with_embedding)
+
+  eval_task_spec = eval_curriculum.task_spec[:100] # for example
+  eval_task_spec_with_embedding = task_encoder.get_task_embedding(eval_task_spec)
 
   if use_elm:
-    generator = OpenELMTaskGenerator(train_tasks)
+    task_generator = OpenELMTaskGenerator(LLM_MODEL_PATH)
 
-  sampler = SyllabusTaskSampler(train_tasks)
-
-  # This will likely have to work off of a pretrained model
-  model = load_model(pretrained_model_path='')
-
-  for epoch in range(3): # epochs that run in 8 A100 hours
+  # epochs that run in 8 A100 hours
+  for epoch in range(3):
     if use_elm and epoch % epochs_per_elm_update == 0:
       # Coordinate with Ryan to figure out how to do this
       # The one issue is that changing the tasks will
       # mess up the existing curriculumg heuristics
       # Possibly ELM runs infrequently? Make sense,
       # we should get a lot out of each ELM update
-      train_tasks = generator.permute_tasks(train_tasks)
-      sampler.update_tasks(train_tasks)
+      train_task_spec = task_generator.permute_tasks(train_task_spec)
+      train_task_spec_with_embedding = task_encoder.get_task_embedding(train_task_spec)
+      task_sampler.add_new_tasks(train_task_spec_with_embedding)
 
       # Sample one task per team. Note that the trainer runs many
       # envs in parallel, and these are vectorized
-      batch_of_tasks = sampler.sample_tasks(num_tasks=64)
+      batch_task_spec_with_embedding = task_sampler.sample_tasks(num_tasks=64)
 
     # Joseph will provide an API for training and evaluation with CleanRL
-    model, train_stats = train_on_tasks(model, batch_of_tasks)
+    # David has specific ideas on how to pass around the embedding in and out of the env
+    agent_model, train_stats = train_on_tasks(agent_model, batch_task_spec_with_embedding)
 
     # Sampler updates its heuristics based on performance delta
-    sampler.update(batch_of_tasks, train_stats)
+    task_sampler.update(batch_task_spec_with_embedding, train_stats)
 
     # We also want to check performance on held-out tasks
     # This is just a split from the heuristics and is separate from
     # the competition evaluation tasks used to score models
-    eval_stats = evaluate_on_tasks(model)
+    eval_stats = evaluate_on_tasks(agent_model, eval_task_spec_with_embedding)
     wandb.log(train_stats, eval_stats)
