095c820084a3f35ac1f343712aa1a5c29eafc896
Nikhil Pinnaparaju <nikhilpinnaparaju@gmail.com>
2023-06-26 19:01:58 -0500

    Task Embedder works and prompt creation

--- curriculum/train_helper.py
+++ curriculum/train_helper.py
@@ -1,12 +1,16 @@
 import inspect
 import random
 import numpy as np
+import ast
+from tqdm import tqdm
+import json
 
 from elm_curriculum_gen import OpenELMCurriculumGenerator
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, CodeGenModel
 
 import nmmo.task
+from static_src_dict import src_mapping
 
 ######################################################################
 # to be provided by Joseph
@@ -34,7 +38,8 @@ def load_agent_model(model_path):
 class TaskEmbeddingGenerator:
   def __init__(self, checkpoint): # OpenELM default
     # https://huggingface.co/docs/transformers/model_doc/codegen#how-to-use
-    self.model = AutoModelForCausalLM.from_pretrained(checkpoint)
+    # self.model = AutoModelForCausalLM.from_pretrained(checkpoint)
+    self.model = CodeGenModel.from_pretrained(checkpoint)
     self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
     # eval func definitions used in task spec, necessary for encoding the task
@@ -44,40 +49,42 @@ class TaskEmbeddingGenerator:
 
   def update_context(self, eval_fn_code):
     self.eval_fn_code = eval_fn_code
-    self.prompt_template = self._contruct_prompt_template()
 
-  def _contruct_prompt_template(self):
-    assert self.eval_fn_code is not None, 'eval_fn_code must be provided'
-    # something like this?
-    self.prompt_template = f"""
-      Your goal is to explain what an agent must accomplish in the neural nmmo,
-      which is expressed as a python function, <the function name>
+  def _construct_prompt(self, reward_to, eval_fn, eval_fn_kwargs):
+    eval_src = inspect.getsource(eval_fn)
+    called_functions = get_called_functions(eval_src)
+    aux_src = "\n\n".join([src_mapping.get(call, '') for call in called_functions])
 
-      The neural mmo is ... explain the game
+    eval_fn_kwargs = str(eval_fn_kwargs)
+    # plug in these args to the prompt template, which will be fed into the model
 
-      <the function name> can use these data structure: gs, subject
+    task_specific_prompt = f"""Your goal is to explain what an agent must accomplish in the neural nmmo,
+      which is expressed as a python function.
+      Neural MMO is a computationally accessible, open-source research platform that 
+      simulates populations of agents in virtual worlds. We challenge you to train a
+      team of agents to complete tasks they have never seen before against opponents
+        they have never seen before on maps they have never seen before.
+    
+      The reward from this function goes to {reward_to}.
+      
+      The function name is {eval_fn.__name__}. These are the arguments that the function takes {eval_fn_kwargs}.
+    
+      The function source code is {eval_src}.
 
-      <the function name> can use these other functions
-      {self.eval_fn_code}
-
-      The source code of <the function name> is:
-
-      The agent's goal has been defined with <the function name> and
-      these parameters: <kwargs that go into the function>
+      This function calls these other functions {aux_src}.
 
       Explain step by step, and as accurately as possible,
 
       The agent's goal is"""
-    print()
+    
+    return task_specific_prompt
 
-  def _construct_prompt(self, reward_to, eval_fn, eval_fn_kwargs):
-    # plug in these args to the prompt template, which will be fed into the model
-    pass
+  def get_task_embedding(self, task_spec, to_file=False):
+    if to_file:
+      output_file = open("task_embedding_file.json", "w+")
 
-  def get_task_embedding(self, task_spec):
-    #assert self.prompt_template is not None, 'prompt_template must be set'
     task_spec_with_embedding = []
-    for single_spec in task_spec:
+    for single_spec in tqdm(task_spec):
       if len(single_spec) == 3:
         reward_to, eval_fn, eval_kwargs = single_spec
         task_kwargs = {}
@@ -88,10 +95,24 @@ class TaskEmbeddingGenerator:
         raise ValueError('len(single_spec) must be either 3 or 4')    
 
       # TODO: make the below lines run
-      #prompt = self._construct_prompt(reward_to, eval_fn, kwargs)
-      #embedding = self.model.get_hidden_layer(prompt) # something like this? 
-      task_kwargs['embedding'] = np.array([1,2,3,4]) # dummy
-      task_spec_with_embedding.append((reward_to, eval_fn, eval_kwargs, task_kwargs))
+      prompt = self._construct_prompt(reward_to, eval_fn, eval_kwargs)
+      tokens =  self.tokenizer(prompt, return_tensors="pt", truncation=True)
+      embedding = self.model(**tokens)[0].mean(dim=1)
+      task_kwargs['embedding'] = embedding
+      # task_spec_with_embedding.append((reward_to, eval_fn, eval_kwargs, task_kwargs))
+
+      if to_file:
+        line_data = {
+                'reward_to': reward_to,
+                'eval_fn': eval_fn.__name__,
+                'eval_kwargs': str(eval_kwargs),
+                'embedding': embedding[0].tolist()
+            }
+        output_file.write(json.dumps(line_data) + '\n')
+
+    if to_file:
+      output_file.close()
+
     return task_spec_with_embedding
 
 
@@ -160,6 +181,10 @@ class OpenELMTaskGenerator(SimpleTaskGenerator):
     evolved_task_spec = self.elm_curriculum_generator.evolve_tasks(steps=100, task_spec=task_spec)
     return evolved_task_spec
 
+def get_called_functions(source_code):
+    tree = ast.parse(source_code)
+    funcs = [node.func.id for node in ast.walk(tree) if isinstance(node, ast.Call)]
+    return funcs
 
 # how to load functions from str and get the src of fn subset
 """
