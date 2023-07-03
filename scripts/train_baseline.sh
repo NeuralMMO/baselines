fc19db62c9698ca1a74ce00a59d9cec8837b5790
David Bloomin <daveey@gmail.com>
2023-07-02 21:43:09 -0700

    new defaults

--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -u -O -m train \
---rollout.num_envs=8 \
+--rollout.num_envs=12 \
 --rollout.num_buffers=2  \
 --ppo.bptt_horizon=8 \
 --wandb.entity=daveey \
@@ -9,7 +9,9 @@ python -u -O -m train \
 --train.experiments_dir=/fsx/home-daveey/experiments \
 --train.num_steps=10000000000 \
 --env.num_maps=100 \
---env.team_size=8 \
---env.num_teams=16 \
---env.num_learners=16 \
+--env.team_size=1 \
+--env.num_teams=128 \
+--env.num_npcs=256 \
+--reward.environment \
+--ppo.training_batch_size=128 \
 "${@}"
