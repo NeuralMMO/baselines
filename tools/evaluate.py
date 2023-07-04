ba1d978b1f361896312faa7fa41c95d57b3eae7b
David Bloomin <daveey@gmail.com>
2023-07-04 12:19:01 -0700

    cp

--- tools/evaluate.py
+++ tools/evaluate.py
@@ -156,6 +156,7 @@ if __name__ == "__main__":
   )
 
   device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
+  print("Using device: ", device)
 
   policies = []
   policy_names = []
--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import argparse
-import imp
 import logging
 import os
 import re
