6415b15b82807d48c0594677e561b0e4682d6b7c
David Bloomin <daveey@gmail.com>
2023-07-09 16:54:39 -0700

    cp

--- scripts/train_baseline.sh
+++ scripts/train_baseline.sh
@@ -6,5 +6,5 @@ python -u -O -m train \
 --train.runs_dir=/fsx/home-daveey/runs/ \
 --train.policy_store_dir=/fsx/home-daveey/policies/ \
 --train.num_steps=10000000000 \
---env.num_maps=100
+--env.num_maps=100 \
 "${@}"
