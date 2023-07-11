Fix NaNs caused by bad attention masking

model/realikun/model.py
@@ -215,9 +215,12 @@ class InteractionBlock(nn.Module):
     mask = torch.cat([x['team_mask'], x['npc_mask'], x['enemy_mask']], dim=-1)
     mask = mask.to(bool).view(bs * na, ne)
     h = h.view(bs * na, ne, nf).transpose(0, 1)
-    h = self.transformer(h, src_key_padding_mask=mask)
+
     # trasformer returns nan when mask is all true
-    h = h.masked_fill(torch.isnan(h), 0)
+    # Masking with zeros produces nan in action sampling
+    mask[mask.all(dim=1)] = False
+    h = self.transformer(h, src_key_padding_mask=mask)
+
     h = h.transpose(0, 1).view(bs, na, ne, nf)
     h = h.max(dim=2)[0]
     return h