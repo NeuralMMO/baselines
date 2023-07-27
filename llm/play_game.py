39d56883969d869735be5a0a260df43b78248fb2
kywch <choe.kyoung@gmail.com>
2023-07-27 22:36:49 +0000

    added player stat

--- llm/play_game.py
+++ llm/play_game.py
@@ -1,30 +1,48 @@
 from pdb import set_trace as T
 from tqdm import tqdm
 
+import numpy as np
+
 import nmmo
 
 from scripted import baselines
-from generated_agent import Agent
-
+from llm.generated_agent import Agent
+from environment import process_event_log
 
 SEED = 42
 
+def get_agent_info(env, agent_id):
+    # some other old info can be accessed via
+    # env.realm.players[agent_id].history
+    task = env.agent_task_map[agent_id][0]
+    log = {
+        "lifetime": env.realm.tick,
+        "task_name": "Default StayAlive task" if task.spec_name is None else task.spec_name,
+        "task_completed": task.completed,
+    }
+    achieved, performed, _ = process_event_log(env.realm, [agent_id])
+    log.update(achieved)
+    log.update(performed)
+    return log
+
 config = nmmo.config.Default()
 config.PLAYERS = [Agent]
-config.LOG_ENV = True
-config.LOG_EVENTS = True
-config.LOG_MILESTONES = True
-config.LOG_VERBOSE = True
-#config.LOG_FILE = 'log.txt'
 
 env = nmmo.Env(config, seed=SEED)
 
+agent_info = []
 obs = env.reset(seed=SEED)
 for t in tqdm(range(128)):
-    obs = env.step({})
+    _, _, d, _ = env.step({})
+    agent_info += [get_agent_info(env, agent_id) for agent_id in d if d[agent_id] is True]
 
-logs = env.realm.log_helper
+# remaining agents
+agent_info += [get_agent_info(env, agent_id) for agent_id in env.realm.players]
 
-
-for key, vals in logs.packet['Milestone'].items():
-    print(f'{key}: Maximum value of {max(vals)}')
+for key in agent_info[0]:
+    if isinstance(agent_info[0][key], (bool, np.bool_)):
+        mean_val = np.mean([info[key] for info in agent_info])
+        print(f"{key}: Mean value of {mean_val}")
+    else:
+      max_val = max([info[key] for info in agent_info])
+      print(f"{key}: Maximum value of {max_val}")
